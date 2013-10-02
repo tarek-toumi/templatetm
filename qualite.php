20da08ca8cde5a3612aef55d26efcc055e0befb7
Tarek TOUMI <tarek.2mi@gmail.com>
2013-10-02 21:33:31 +0100

    Version Beta1

---
+++ qualite.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr-fr" lang="fr-fr" charset="UTF-8">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15" />
+		<title>Tunimarine - Services</title>
+		<link type="text/css" href="styles/reset.css" rel="stylesheet" media="all" />
+		<link type="text/css" href="styles/text.css" rel="stylesheet" media="all" />
+		<link type="text/css" href="styles/960.css" rel="stylesheet" media="all" />
+		<link type="text/css" href="styles/style.css" rel="stylesheet" media="all" />
+		<link type="text/css" href="styles/services.css" rel="stylesheet" media="all" />
+		<link type="text/css" href="styles/jquery.bxslider.css" rel="stylesheet" />
+		
+		<script type="text/javascript" src="/script/jquery-1.8.2.min.js"></script>
+		<script type="text/javascript" src="/script/code.js"></script>
+	</head>
+	
+	<body>
+		<!--Header-->
+		<?php include 'templates/header.php';?>
+
+		<!--Content-->
+		<div class="content_container">
+			<div class="container_12">
+				<div class="prefix_full_width">
+					<div class="grid_5">
+						<div class="col3">
+							<h2>Assurance Qualité de la fabrication</h2>
+							<p> La nécessité d'améliorer en permanence notre qualité de service client, nous imposent une adaptation constante de nos méthodes de travail. <br />
+								Dans cette optique et pour répondre aux demandes formulées par ses clients, TUNIMARINE s'est engagé dans la mise en place d'une politique d'Assurance Qualité applicable à l'ensemble de ses achats et fabrications. <br />
+								Cette politique est basée sur le principe de la validation de la conformité des matières et des produits au niveau de la fabrication. </p>
+						</div>
+					</div>
+				</div>
+				<div class="prefix_full_width">
+					<div class="grid_5">
+						<div class="col3">
+							<h2>Principe</h2>
+							<p> Toute livraison effectuée aux clients doit être accompagnée des documents attestant la conformité du ou des lots livrés aux exigences de la commande. </p>
+						</div>
+					</div>
+				</div>
+				<div class="prefix_6">
+					<div class="grid_5">
+						<div class="col3">
+							<h2>Laboratoire de contrôle dimensionnel interne</h2>
+							<p>Le Contrôle de la Qualité interne vérifie en permanence les procédés et les produits, conformément aux procédures certifiées. <br />
+							Le Laboratoire interne TUNIMARINE est équipé d'appareils qui permettent d'effectuer nombreux tests de qualité: </p>
+							<ul>
+								<li>Essais mécaniques</li>
+								<li>Contrôle dimensionnel</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="prefix_6">
+					<div class="grid_5">
+						<div class="col3">
+							<center>
+								<img border="0" title="Qualité" alt="Qualité" src="/images/picto_qualite.png" class="image_border_presentation">
+							</center>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		<!--Footer-->
+		<?php include 'templates/footer.php';?>
+	</body>
+</html>
